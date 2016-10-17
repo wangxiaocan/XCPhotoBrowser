c0c14f3dbb0158d33f3ad4aae9d2ea1242c95395
王文科 <xiaocan@wangwenkedeiMac.local>
2016-10-17 14:40:17 +0800

    Initial Commit

---
+++ XCPhotoBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCPhotoBrowser
+//
+//  Created by 王文科 on 2016/10/17.
+//  Copyright © 2016年 xiaocan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XCPhotoBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XCPhotoBrowser
+//
+//  Created by 王文科 on 2016/10/17.
+//  Copyright © 2016年 xiaocan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
